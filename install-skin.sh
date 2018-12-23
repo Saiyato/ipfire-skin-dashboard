15033c1d08ec081a3fd52f7435ced814ce07d964
Rachid Groeneveld <rachidgroeneveld@hotmail.nl>
2018-12-23 19:12:54 +0100

    Update install-skin.sh
    Removed last reference of timfprogs' script in the script itself.

--- install-skin.sh
+++ install-skin.sh
@@ -12,7 +12,6 @@ if [[ ! -d $skindir/include/webfonts ]]; then mkdir -p $skindir/include/webfonts
 # Download the manifest
 
 wget "https://raw.githubusercontent.com/Saiyato/ipfire-skin-dashboard/master/MANIFEST"
-wget "https://github.com/timfprogs/ipfblocklist/raw/master/MANIFEST"
 
 # Download and move files to their destinations
 echo Downloading files...
